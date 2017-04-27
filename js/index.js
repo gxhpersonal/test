d21f014647d77171b0682d5aa4ea7c0bda4247f8
guoxh <guoxh@huizuche.com>
2017-04-27 14:12:56 +0800

    test 动画

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var testApp = angular.module("testApp",[]);
+	testApp.controller("testAppController",["$scope","$http",function($scope,$http){
+    var btn = document.getElementById('btn');
+		btn.onclick = function(){
+			alert(111)
+		}
+
+		var u = navigator.userAgent;
+		btn.innerHTML = u;
+        var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    //     setTimeout(function(){
+    //     	alert('是否是Android：'+isAndroid);
+    //     alert('是否是iOS：'+isiOS);
+    // },1000)
+
+	//点击下一步时验证报错
+    $scope.clickError = function (thisName, errTxt1, errTxt2) {
+        var $this = $('#' + thisName),_thisVal = $this.val();
+        //document.addEventListener("animationend", function (t) {
+        //    $this.closest(".redbor").removeClass("active")
+        //})
+        //红框报错动画
+        $this.closest(".redbor").addClass("active");
+        window.setTimeout(function (e) {
+            $this.closest(".redbor").removeClass("active")
+        }, 1000);
+        //顶部报错动画
+        if (!_thisVal) {
+            $('.error-tc').html(errTxt1).addClass("slide");
+            window.setTimeout(function (e) {
+                $('.error-tc').removeClass("slide");
+            }, 2500);
+        } else {
+            $('.error-tc').html(errTxt2).addClass("slide");
+            window.setTimeout(function (e) {
+                $('.error-tc').removeClass("slide");
+            }, 2500);
+        }
+    }
+	}])
